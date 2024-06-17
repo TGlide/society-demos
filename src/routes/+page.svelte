3ebd76188c508794a621d765ec43aec2e147dd07
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2024-06-18 00:51:18 +0100

    demo 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,63 @@
 <script lang="ts">
-	import { CurrencyEur, Empty, ArrowBendDownLeft, X } from "phosphor-svelte";
+	import { CurrencyEur, ArrowBendDownLeft, X } from "phosphor-svelte";
 	import { flip } from "svelte/animate";
-	import { linear } from "svelte/easing";
 	import { fade } from "svelte/transition";
 
 	let value = $state<number>();
 	const disabled = $derived(typeof value !== "number");
-	const gastos = $state<number[]>([10, 20, 30]);
+
+	type Gasto = {
+		value: number;
+		id: number;
+	};
+	const gastos = $state<Gasto[]>([]);
+	const total = $derived(gastos.reduce((a, b) => a + b.value, 0));
 
 	function onsubmit(e: SubmitEvent) {
 		e.preventDefault();
 		if (!value) return;
-		gastos.push(value);
+
+		gastos.push({ value, id: Date.now() });
 		value = undefined;
 	}
+
+	$effect(() => {
+		if (total >= 5000) {
+			console.log("aaaaaaaaaaaaaaaaaa");
+		}
+	});
 </script>
 
-<div class="mx-auto w-[600px] p-8">
+<div class="mx-auto flex h-screen w-[600px] flex-col p-8">
 	<h1 class="text-4xl font-bold">Gastos</h1>
 
 	<form
 		{onsubmit}
-		class="mt-4 flex items-center gap-2 overflow-hidden rounded-2xl border-2 border-black"
+		class="mt-4 flex shrink-0 items-center gap-2 overflow-hidden rounded-2xl border-2 border-black"
 	>
 		<div class="border-r-2 border-black bg-orange-500/20 p-3">
 			<CurrencyEur size={24} weight="bold" />
 		</div>
 		<input type="number" placeholder="0.00" class="grow text-xl outline-none" bind:value />
-		<button class="pr-4 transition-all" class:opacity-50={disabled} {disabled}>
+		<button
+			class="p-2 {disabled ? '' : 'rounded-full bg-black text-white'} mr-4 transition-all"
+			class:opacity-50={disabled}
+			{disabled}
+		>
 			<ArrowBendDownLeft size={18} weight="bold" />
 		</button>
 	</form>
 
-	{#if gastos.length}
-		<ul class="relative m-4" transition:fade>
-			{#each gastos as gasto, idx (gasto)}
+	<div class="flex grow flex-col justify-between overflow-y-clip">
+		<ul class="relative grow overflow-y-auto">
+			{#each gastos as gasto, idx (gasto.id)}
 				<li
-					class="flex w-full items-center gap-1 border-t-2 border-gray-300 py-4 text-xl font-semibold first:border-t-0"
+					class=" flex w-full items-center gap-1 border-t-2 border-gray-200 px-2 py-4 text-xl font-semibold first:border-t-0"
 					animate:flip={{ duration: 500 }}
-					out:fade={{ duration: 200 }}
+					transition:fade={{ duration: 150 }}
 				>
-					<CurrencyEur size={20} weight="bold" />
-					<span>{gasto.toFixed(2)}</span>
+					<CurrencyEur size={24} weight="bold" />
+					<span>{gasto.value.toFixed(2)}</span>
 					<button
 						class="ml-auto grid rounded-xl border-2 p-2 text-gray-300 transition hover:border-red-300 hover:text-red-300"
 						onclick={() => gastos.splice(idx, 1)}
@@ -51,12 +67,17 @@
 				</li>
 			{/each}
 		</ul>
-	{:else}
-		<div class="mt-8 flex flex-col items-center gap-2 text-gray-500">
-			<Empty size={48} />
-			<p class="text-center text-xl font-semibold">Sem gastos para mostrar</p>
+
+		<div class="mt-auto flex items-center justify-between border-t-2 border-black pt-4">
+			<h2 class="text-3xl font-bold">Total</h2>
+			{#key total}
+				<p class="gelatine flex items-center text-3xl font-bold">
+					<CurrencyEur size={40} weight="fill" />
+					<span>{total.toFixed(2)}</span>
+				</p>
+			{/key}
 		</div>
-	{/if}
+	</div>
 </div>
 
 <style>
@@ -66,17 +87,17 @@
 			transform: scale(1, 1);
 		}
 		25% {
-			transform: scale(0.9, 1.1);
+			transform: scale(0.95, 1.05);
 		}
 		50% {
-			transform: scale(1.1, 0.9);
+			transform: scale(1.05, 0.95);
 		}
 		75% {
-			transform: scale(0.95, 1.05);
+			transform: scale(0.975, 1.025);
 		}
 	}
 
-	li {
+	.gelatine {
 		animation: gelatine 0.3s ease;
 	}
 </style>
