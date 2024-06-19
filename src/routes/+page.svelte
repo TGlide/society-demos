6a39f6d8e64871dcd60d340ccef4bf73c58bd065
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2024-06-20 00:35:20 +0100

    initial stage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import { CurrencyEur } from "phosphor-svelte";
-	import { conta } from "$lib/conta.svelte";
 </script>
 
 <div class="mx-auto flex h-screen w-[600px] flex-col p-8">
 	<h1 class="text-4xl font-bold">Olá Thomas!</h1>
 	<p class="flex items-center mt-2 text-xl">
 		O total de gastos atual é de
-		<span class="inline-flex items-center pl-1"><CurrencyEur  weight="bold" /> {conta.total.toFixed(2)}</span>
+		<span class="inline-flex items-center pl-1"><CurrencyEur  weight="bold" /> 0.00</span>
 	</p>
 
 	<a href="/conta" class="mt-4 flex gap-2 underline hover:opacity-50">
--- src/routes/conta/+page.svelte
+++ src/routes/conta/+page.svelte
@@ -1,77 +1,177 @@
 <script lang="ts">
 	import { CurrencyEur, ArrowBendDownLeft, X } from "phosphor-svelte";
-	import { flip } from "svelte/animate";
-	import { fade } from "svelte/transition";
-	import { conta } from "$lib/conta.svelte";
-
-	let value = $state<number>();
-	const disabled = $derived(typeof value !== "number");
-
-	function onsubmit(e: SubmitEvent) {
-		e.preventDefault();
-		if (!value) return;
-
-		conta.gastos.push({ value, id: Date.now() });
-		value = undefined;
-	}
 </script>
 
-<div class="mx-auto flex h-screen w-[600px] flex-col p-8">
-	<div class="flex items-baseline justify-between">
-		<h1 class="text-4xl font-bold">Gastos</h1>
-		<a href="/" class="flex items-center gap-1 text-lg underline hover:opacity-75"> Home </a>
+<div class="container">
+	<div class="header">
+		<h1>Gastos</h1>
+		<a href="/">Home</a>
 	</div>
 
-	<form
-		{onsubmit}
-		class="mt-4 flex shrink-0 items-center gap-2 overflow-hidden rounded-2xl border-2 border-black"
-	>
-		<div class="border-r-2 border-black bg-orange-500/20 p-3">
+	<form>
+		<div class="icon-container">
 			<CurrencyEur size={24} weight="bold" />
 		</div>
-		<input type="number" placeholder="0.00" class="grow text-xl outline-none" bind:value />
-		<button
-			class="p-2 {disabled ? '' : 'rounded-full bg-black text-white'} mr-4 transition-all"
-			class:opacity-50={disabled}
-			{disabled}
-		>
+		<input type="number" placeholder="0.00" />
+		<button class="submit-button">
 			<ArrowBendDownLeft size={18} weight="bold" />
 		</button>
 	</form>
 
-	<div class="flex grow flex-col justify-between overflow-y-clip">
-		<ul class="relative grow overflow-y-auto">
-			{#each conta.gastos as gasto, idx (gasto.id)}
-				<li
-					class=" flex w-full items-center gap-1 border-t-2 border-gray-200 px-2 py-4 text-xl font-semibold first:border-t-0"
-					animate:flip={{ duration: 500 }}
-					transition:fade={{ duration: 150 }}
-				>
-					<CurrencyEur size={24} weight="bold" />
-					<span>{gasto.value.toFixed(2)}</span>
-					<button
-						class="ml-auto grid rounded-xl border-2 p-2 text-gray-300 transition hover:border-red-300 hover:text-red-300"
-						onclick={() => conta.gastos.splice(idx, 1)}
-					>
-						<X size={18} weight="bold" />
-					</button>
-				</li>
-			{/each}
+	<div class="content">
+		<ul>
+			<li>
+				<CurrencyEur size={24} weight="bold" />
+				<span>10.00</span>
+				<button >
+					<X size={18} weight="bold" />
+				</button>
+			</li>
 		</ul>
 
-		<div class="mt-auto flex items-center justify-between border-t-2 border-black pt-4">
-			<h2 class="text-3xl font-bold">Total</h2>
-			{#key conta.total}
-				<p class="gelatine flex items-center text-3xl font-bold">
-					<CurrencyEur size={40} weight="fill" />
-					<span>{conta.total.toFixed(2)}</span>
-				</p>
-			{/key}
+		<div class="total">
+			<h2 class="total-title">Total</h2>
+			<p class="total-amount gelatine">
+				<CurrencyEur size={40} weight="fill" />
+				<span>0.00</span>
+			</p>
 		</div>
 	</div>
 </div>
 
 <style>
+	.container {
+		margin: 0 auto;
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+		width: 600px;
+		padding: 2rem;
+	}
+
+	.header {
+		display: flex;
+		align-items: baseline;
+		justify-content: space-between;
+	}
+
+	h1 {
+		font-size: 2.25rem;
+		line-height: 2.5rem;
+		font-weight: bold;
+	}
+
+	a {
+		font-size: 1.125rem;
+		line-height: 1.75rem;
+		text-decoration: underline;
+	}
+
+	a:hover {
+		opacity: 0.75;
+	}
+
+	form {
+		display: flex;
+		flex-shrink: 0;
+		align-items: center;
+		gap: 0.5rem;
+		border-radius: 1rem;
+		border: 2px solid black;
+		margin-top: 1rem;
+		overflow: hidden;
+	}
+
+	.icon-container {
+		border-right: 2px solid black;
+		background-color: #FEE3D0;
+		padding: 0.75rem;
+	}
+
+	form input {
+		flex-grow: 1;
+		font-size: 1.25rem;
+		outline: none;
+	}
+
+	.submit-button {
+		margin-right: 1rem;
+		border-radius: 9999px;
+		background: black;
+		padding: 0.5rem;
+		color: white;
+		transition: 0.2s ease;
+	}
+
+	.submit-button:hover {
+		opacity: 0.75;
+	}
+
+	.content {
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
+		justify-content: space-between;
+		overflow-y: hidden;
+	}
+
+	ul {
+		position: relative;
+		flex-grow: 1;
+		overflow-y: auto;
+	}
+
+	li {
+		display: flex;
+		width: 100%;
+		align-items: center;
+		gap: 0.25rem;
+		border-top: 2px solid #e5e7eb;
+		padding: 1rem 0.5rem;
+		font-size: 1.25rem;
+		font-weight: 600;
+	}
+
+	li:first-child {
+		border-top: none;
+	}
+
+	li button {
+		margin-left: auto;
+		display: grid;
+		border-radius: 0.75rem;
+		border: 2px solid;
+		padding: 0.5rem;
+		color: #d1d5db;
+		transition: 0.2s ease;
+	}
+
+	li button:hover {
+		border-color: #f87171;
+		color: #f87171;
+	}
+
+	.total {
+		margin-top: auto;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		border-top: 2px solid black;
+		padding-top: 1rem;
+	}
+
+	.total-title {
+		font-size: 1.875rem;
+		font-weight: bold;
+	}
+
+	.total-amount {
+		display: flex;
+		align-items: center;
+		font-size: 1.875rem;
+		font-weight: bold;
+	}
+
 	@keyframes gelatine {
 		from,
 		to {
